d910a3072e8f41f29aca6d77c8ec570a0a7c57de
madhavparekh <madhavparekh@gmail.com>
2018-03-25 16:07:34 -0700

    logs output if flaged - reads file and performs search

--- liri.js
+++ liri.js
@@ -9,8 +9,97 @@ var spotOffset = 0;
 
 var Request = require('request');
 var Inquirer = require('inquirer');
+var fs = require('fs');
+var file = './logs/logcat.txt';
 
-var prompt = () => {
+var logOutPut = false;
+var doConti = true;
+var separator = `########\n`;
+
+var userInput = process.argv.slice(2);
+// console.log(userInput);
+
+if (userInput.length === 0) {
+  // console.log(`Running Node`);
+  prompt();
+} else {
+  if (userInput[userInput.length - 1] === '-log') {
+    logOutPut = true;
+    var searchStr = arrayParser(userInput.slice(1, userInput.length - 1));
+  } else var searchStr = arrayParser(userInput.slice(1));
+
+  doSwitch(userInput[0], searchStr);
+}
+
+function doSwitch(api, searchStr) {
+  switch (api) {
+    case 'songs':
+      fetchSpotify(searchStr);
+      break;
+    case 'tweets':
+      getTweets(searchStr);
+      break;
+    case 'movies':
+      getMovies(searchStr);
+      break;
+    case 'help':
+      helpInfo();
+      break;
+    case 'file':
+      readFile();
+      doConti = false;
+      break;
+  }
+}
+
+function readFile() {
+  fs.readFile(userInput[1], 'utf8', (err, data) => {
+    console.log('reading file');
+    if (err) console.log(err);
+    else {
+      // console.log(data);
+      var list = data.split('\r');
+      list.forEach((e) => {
+        var search = e.trim().split(',');
+
+        setTimeout(() => {
+          doSwitch(search[0], search[1]);
+        }, 1000);
+      });
+
+      if (!doConti) doContinue();
+      // console.log(list);
+    }
+  });
+}
+
+function helpInfo() {
+  console.group();
+  console.log(
+    `node liri.js [songs|tweets|movies|file|help] [search param] [-log]`
+  );
+  console.log(`             Params are optional`);
+  console.log(`             [songs] <title|artist|album|genera>`);
+  console.log(`             tweets <search text>`);
+  console.log(`             movies <title>`);
+  console.log(`             file <filepath>`);
+  console.log(`             Enter no params for prompt mode (inquirer)`);
+  console.log(
+    `             -log as last param to log results in ./logs/logcat.txt`
+  );
+  console.groupEnd();
+}
+
+function arrayParser(arry) {
+  var parsedStr = '';
+  arry.forEach((e) => {
+    parsedStr += e + ' ';
+  });
+  // console.log(parsedStr);
+  return parsedStr.trim();
+}
+
+function prompt() {
   Inquirer.prompt([
     {
       type: 'list',
@@ -31,13 +120,12 @@ var prompt = () => {
       case 'Songs':
         fetchSpotify(data.param);
         break;
-      case 'Movie':
+      case 'Movies':
+        getMovies(data.param);
         break;
     }
   });
-};
-
-prompt();
+}
 
 function doContinue() {
   Inquirer.prompt([
@@ -67,20 +155,29 @@ function fetchSpotify(search) {
   };
 
   spotify.search(params, function(err, data) {
+    var strOut = separator;
     if (err) {
-      return console.log('Error occurred: ' + err);
+      strOut += `Error occurred: ${err}\n`;
     }
     data.tracks.items.forEach((e) => {
-      console.log(`Song   : ${e.name}`);
+      // strOut += e);
+      strOut += `  Song    : ${e.name}\n`;
+      strOut += `  Album   : ${e.album.name}\n`;
       var art = '';
       e.artists.forEach((artist) => {
         art += artist.name + ', ';
       });
-      console.log(`Artist : ${art}`);
-      console.log(`Preview: ${e.preview_url}`);
-      console.log(`----`);
+      strOut += `  Artist  : ${art}\n`;
+      strOut += `  Preview : ${e.preview_url}\n`;
+      strOut += `  ----\n`;
     });
-    doContinue();
+    strOut += separator;
+
+    console.log(strOut);
+
+    if (logOutPut) logOutPutFile(strOut);
+
+    if (doConti) doContinue();
   });
 }
 
@@ -90,18 +187,77 @@ function getTweets(search) {
   var params = {
     q: search || 'donald trump',
     tweet_mode: 'extended',
-    result_type: 'popular',
+    result_type: 'recent',
   };
 
   client.get('search/tweets', params, function(error, tweets, response) {
+    var strOut = separator;
     if (error) console.log(error);
     else {
-      //   console.log(tweets);
       tweets.statuses.forEach((element, indx) => {
-        console.log(`Tweet ${indx + 1} : ${element.full_text}`);
-        console.log(`---`);
+        // console.log(element);
+        strOut += `  Tweeted by  : ${element.user.name}\n`;
+        strOut += `  Screen Name : ${element.user.screen_name}\n`;
+        strOut += `  Tweet       : ${element.full_text}\n`;
+        strOut += `  ++++\n`;
       });
     }
-    doContinue();
+    strOut += separator;
+    console.log(strOut);
+    if (logOutPut) logOutPutFile(strOut);
+
+    if (doConti) doContinue();
+  });
+}
+
+function getMovies(search) {
+  var key = 'trilogy';
+  search = search || 'Get Out';
+
+  var url = `http://www.omdbapi.com/?apikey=${key}&t=${search}`;
+
+  Request(url, (err, resp, data) => {
+    var strOut = separator;
+    if (err) console.log(err);
+    else {
+      var movieData = JSON.parse(data);
+      // console.log(movieData);
+      if (movieData.Response !== 'False') {
+        strOut += `  Title   : ${movieData.Title}\n`;
+        strOut += `  Year    : ${movieData.Year}\n`;
+        strOut += `  Country : ${movieData.Country}\n`;
+        strOut += `  Language: ${movieData.Language}\n`;
+        strOut += `  Actors  : ${movieData.Actors}\n`;
+        if (movieData.Ratings) {
+          var ratings = `  Ratings : `;
+          movieData.Ratings.forEach((e, i) => {
+            var moreRatings = '';
+
+            if (i === 0) ratings += `${e.Source} - ${e.Value}`;
+            else {
+              moreRatings += `\n            ${e.Source} - ${e.Value}`;
+              ratings += moreRatings;
+            }
+          });
+          strOut += ratings + '\n';
+        }
+
+        strOut += `  Plot    : ${movieData.Plot}\n`;
+      } else {
+        strOut += `  ${search} - ${movieData.Error}\n`;
+      }
+    }
+    strOut += separator;
+    console.log(strOut);
+    if (logOutPut) logOutPutFile(strOut);
+
+    if (doConti) doContinue();
+  });
+}
+
+function logOutPutFile(str) {
+  str = new Date() + '\n' + str;
+  fs.appendFile(file, str, (err) => {
+    if (err) console.log(err);
   });
 }
